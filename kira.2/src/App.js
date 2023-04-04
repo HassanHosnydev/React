9735bf3a525b51558148fd3caee8c8b6051ecaef
ahmedaminahmed_amin <ahmedamineashad@gmail.com>
2023-04-04 03:45:24 +0200

    Branch 1

--- kira.2/src/App.js
+++ kira.2/src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-    <p>Update My Project</p>
+    <p>NEW Branch</p>
     <h1 className="name">Hassan Hosny</h1>
     <h2 className="age">23 years old </h2>
     </>
